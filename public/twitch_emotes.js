ebc1a13041985c6c5c76eb3862bc2eb94e76207f
Evan Heaton <epheat07+github@gmail.com>
2016-09-17 16:16:57 -0400

    added a frame

--- public/twitch_emotes.js
+++ public/twitch_emotes.js
@@ -15,10 +15,11 @@ function preload() {
   OpieOP = loadImage("./TwitchEmotes/OpieOP.png");
   SeemsGood = loadImage("./TwitchEmotes/SeemsGood.png");
   WutFace = loadImage("./TwitchEmotes/WutFace.png");
+  frame = loadImage("frame.png");
 }
 
 function setup() {
-  createCanvas(1250,600);
+  createCanvas(1250,750);
   background(200,200,200,255);
   frameRate(50);
 }
@@ -26,10 +27,12 @@ function setup() {
 var j = 0;
 function draw() {
   background(200,200,200,255);
+  fill(255,220,200,255);
+  quad(80,40,80+900,40,80+900,40+600,80,40+600);
   j = ((j+1)%25);
-  for(var i=0;i<90;i++){
+  for(var i=0;i<58;i++){
     push();
-    translate(13*i,40+30*sin((i+j)/4));
+    translate(80+13*i,120+30*sin((i+j)/4));
     image(Kappa,0,0,80,80);
     image(BrokeBack,0,60,80,80);
     image(OhMyDog,0,120,80,80);
@@ -40,5 +43,7 @@ function draw() {
     image(WutFace,0,420,80,80);
     pop();
   }
+  image(frame,0,0,1100,750);
+
 
 }
